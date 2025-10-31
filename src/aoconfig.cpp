2e06878c4eb99da8c9005a1dff29bcb32e35ddc7
Alex Noir <varsash@gmail.com>
2025-10-31 04:26:08 +0300

    Make always_pre default OFF

--- src/aoconfig.cpp
+++ src/aoconfig.cpp
@@ -192,7 +192,7 @@ void AOConfigPrivate::load_file()
   manual_timeofday = cfg.value("timeofday").toString();
   manual_timeofday_selection = cfg.value("manual_timeofday", false).toBool();
   searchable_iniswap = cfg.value("searchable_iniswap", true).toBool();
-  always_pre = cfg.value("always_pre", true).toBool();
+  always_pre = cfg.value("always_pre", false).toBool();
   chat_tick_interval = cfg.value("chat_tick_interval", 60).toInt();
   chat_ratelimit = cfg.value("chat_ratelimit", 500).toInt();
   emote_preview = cfg.value("emote_preview", true).toBool();
